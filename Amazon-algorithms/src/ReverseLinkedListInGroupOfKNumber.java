cdc036ac1497a4d23abb41745063acb6a1427012
Aqib_Javed <aqibbutt3078@gmail.com>
2019-10-11 21:07:30 +0200

    Initial commit

--- Amazon-algorithms/src/ReverseLinkedListInGroupOfKNumber.java
+++ Amazon-algorithms/src/ReverseLinkedListInGroupOfKNumber.java
@@ -2,34 +2,75 @@ import java.util.Objects;
 
 class LinkedList<T> {
 
-	private Node<T> currentNode;
+	private Node<T> end;
+	private Node<T> start;
 
 	public void push(T t) {
-		if (Objects.isNull(currentNode)) {
-			currentNode = new Node<>(t);
-			currentNode.previous = null;
+		if (Objects.isNull(start)) {
+			addFirst(t);
 		} else {
-			currentNode.next = new Node<>(t);
-			currentNode.next.previous = currentNode;
-			currentNode = currentNode.next;
+			doubleLinkedNode(t);
+
 		}
 	}
 
-	public T pop(T t) {
-		T poppedVal = currentNode.value;
-		currentNode = currentNode.previous;
-		return poppedVal;
+	private void doubleLinkedNode(T t) {
+		end.next = new Node<>(t);
+		end.next.previous = end;
+		end = end.next;
+	}
+
+	private void addFirst(T t) {
+		start = new Node<>(t);
+		end = start;
+	}
+
+	public T pop() {
+		Node<T> popNode = end;
+		end = end.previous;
+		return popNode.value;
+	}
+
+	public T poll() {
+		Node<T> pollNode = start;
+		start = start.next;
+		return pollNode.value;
 	}
 
 	@Override
 	public String toString() {
-		return "LinkedList [currentNode=" + currentNode + "]";
+		Node<T> temp = end;
+		String data = "";
+		if(!Objects.isNull(temp))
+			data = "[";
+		while (!Objects.isNull(temp)) {
+			data += temp.value + ", ";
+			temp = temp.previous;
+		}
+		if(!data.isEmpty())
+		data = data.substring(0, data.length() - 2) + "]";
+		return data;
+	}
+	
+	public String fifoString() {
+		Node<T> temp = start;
+		String data = "";
+		if(!Objects.isNull(temp))
+			data = "[";
+		while (!Objects.isNull(temp)) {
+			data += temp.value + ", ";
+			temp = temp.next;
+		}
+		if(!data.isEmpty())
+		data = data.substring(0, data.length() - 2) + "]";
+		return data;
 	}
 
 	@SuppressWarnings("hiding")
 	class Node<T> {
 		private Node<T> next;
 		private Node<T> previous;
+		private Integer index;
 		private T value;
 
 		public Node() {
@@ -37,14 +78,6 @@ class LinkedList<T> {
 		}
 
 		public Node(T value) {
-			this.value = value;
-		}
-
-		public T getValue() {
-			return value;
-		}
-
-		public void setValue(T value) {
 			this.value = value;
 		}
 
@@ -64,20 +97,33 @@ class LinkedList<T> {
 			this.previous = previous;
 		}
 
-		@Override
-		public String toString() {
-			return "Node [next=" + next + ", previous=" + previous + ", value=" + value + "]";
+		public T getValue() {
+			return value;
 		}
 
+		public void setValue(T value) {
+			this.value = value;
+		}
+
+		public Integer getIndex() {
+			return index;
+		}
+
+		public void setIndex(Integer index) {
+			this.index = index;
+		}
 	}
 }
 
 public class ReverseLinkedListInGroupOfKNumber {
 	public static void main(String[] args) {
 		LinkedList<Integer> li = new LinkedList<>();
-			li.push(1);
-			li.push(2);
-			li.push(3);
-			System.out.println(li);
+		li.push(1);
+		li.push(2);
+		li.push(3);
+		li.push(4);
+		li.push(5);
+		li.poll();
+		System.out.println(li);
 	}
 }
